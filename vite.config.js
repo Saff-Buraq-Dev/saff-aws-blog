31f44262e282c49223668db5ec67f1977d54156f
Saff-Buraq-Dev <gharbi.safwen@hotmail.com>
2024-05-29 15:06:33 -0400

    correct 404 behavior

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,11 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
     base: "/",
     plugins: [
-        vue({
+        vue({}),
+        move({
+            patterns: [
+                { from: '404.html', to: 'dist/404.html' }
+            ]
         })
     ],
     resolve: {
