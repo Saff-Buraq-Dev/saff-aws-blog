f90131e9e53db8f5c7afe8ac433ca910258e93ba
Saff-Buraq-Dev <gharbi.safwen@hotmail.com>
2024-10-22 22:45:11 -0400

    ask me

---
+++ src/components/Pages/AskmePage.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <Navbar />
+        <div class="page-title-area">
+            <div class="container">
+                <div class="page-title-content">
+                    <iframe src="https://b6qbphhn.chat.qbusiness.us-east-1.on.aws/" frameborder="0"></iframe>
+                </div>
+            </div>
+        </div>
+        <Footer />
+    </div>
+</template>
+
+<script>
+import Navbar from '../Layouts/Navbar.vue';
+import Footer from '../Layouts/Footer.vue';
+
+export default {
+    name: 'BlogSearchPage',
+    components: {
+        Navbar,
+        Footer,
+    },
+};
+</script>
